1d0a6e35ae594383c37aea7be744ed66852b6f23
COPS-SB <145305405+COPS-SB@users.noreply.github.com>
2025-07-18 12:58:38 +1000

    Update batch_backup_ext.bat
    - Added loop to remove empty directories that the copy loop created but put no files in

--- Temp/batch_backup_ext.bat
+++ Temp/batch_backup_ext.bat
@@ -4,6 +4,9 @@ title Copy File By EXT
 
 set "source=C:\Temp"
 
+echo Copying all files with desired file extensions...
+echo:
+
 set "extensions=csv doc docm docx dot dotm dotx md odp ods odt pdf pot potm potx ppa ppam pps ppsm ppsx ppt pptm pptx rtf txt wps xla xlam xls xlsb xlsm xlsx xlt xltm xltx xlw xml xps"
 rem testing file extensions set
 rem set "extensions=md pdf txt xml"
@@ -19,7 +22,18 @@ for %%e in (%extensions%) do (
     echo:
 )
 
+echo Removing empty directories...
+for /d /r %%d in (*) do (
+	dir /b %%d | findstr . >nul
+	if errorlevel 1 (
+	    rem echo %%d Folder is empty.
+	    rmdir %%d
+	) else (
+	    rem echo %%d Folder is NOT empty.
+	)
+)
 
 echo:
+echo ----------------------------------------------------------------------------------------------------
 echo End of script
 pause
