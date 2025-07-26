471167d4382dff92a8a81a77ef12d57e768b6a27
WelFedTed <34222508+WelFedTed@users.noreply.github.com>
2025-07-27 08:58:16 +1000

    Update WUReset.bat

--- USB-Tool_Scripts/WUReset.bat
+++ USB-Tool_Scripts/WUReset.bat
@@ -4,7 +4,7 @@ title Reset Windows Update Tool
 
 rem launch correct version depending on architecture (x86 or x64)
 
-set dir="_Data\Apps\Reset Windows Update Tool\"
+set dir="_Data\Apps\Reset Windows Update Tool (wureset)"
 set app=wureset.exe
 
 cd %dir%
