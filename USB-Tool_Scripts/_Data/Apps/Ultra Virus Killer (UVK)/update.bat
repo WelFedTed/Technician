d5cae7129ed7286e88fc848795c7f952728f20a8
WelFedTed <34222508+WelFedTed@users.noreply.github.com>
2025-07-26 21:13:42 +1000

    Create update.bat

---
+++ USB-Tool_Scripts/_Data/Apps/Ultra Virus Killer (UVK)/update.bat
@@ -0,0 +1,13 @@
+@echo off
+
+title App Updater
+
+set "app=Ultra Virus Killer (UVK)"
+set "download=https://www.carifred.com/uvk/UVK.zip"
+
+echo Downloading latest build of %app%...
+
+echo:
+echo End of script
+pause
+exit /b
