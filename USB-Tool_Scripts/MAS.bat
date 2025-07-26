b07f4b463d0aad7ad2674a95070672c830072879
WelFedTed <34222508+WelFedTed@users.noreply.github.com>
2025-07-27 08:34:55 +1000

    Update MAS.bat

--- USB-Tool_Scripts/MAS.bat
+++ USB-Tool_Scripts/MAS.bat
@@ -4,7 +4,7 @@ title Microsoft Activation Scripts
 
 rem launch correct version depending on architecture (x86 or x64)
 
-set dir="_Data\Apps\Microsoft Activiation Scripts (MAS)\MAS\All-In-One-Version-KL"
+set dir="_Data\Apps\Microsoft Activiation Scripts (MAS)\Microsoft-Activation-Scripts-master\MAS\All-In-One-Version-KL"
 set app=MAS_AIO.cmd
 
 cd %dir%
