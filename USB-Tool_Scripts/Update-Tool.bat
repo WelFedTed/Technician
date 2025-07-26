16ca4cd806ed839acd97f617c89b7db5d9a53ac4
WelFedTed <34222508+WelFedTed@users.noreply.github.com>
2025-07-26 23:24:39 +1000

    update files

---
+++ USB-Tool_Scripts/Update-Tool.bat
@@ -0,0 +1,19 @@
+@echo off
+
+title Toole Updater
+
+rem this will update everything on tool as automated as possible
+
+rem update PortableApps
+
+rem call update.bat files for each app
+
+rem update Linux + Windows ISO's
+
+rem update Ventoy
+
+
+echo:
+echo End of script
+pause
+exit /b
