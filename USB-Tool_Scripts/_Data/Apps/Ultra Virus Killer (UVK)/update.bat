52a43291dcfbd34228609a27edf14b3234bae68b
WelFedTed <34222508+WelFedTed@users.noreply.github.com>
2025-07-26 23:24:27 +1000

    Update update.bat

--- USB-Tool_Scripts/_Data/Apps/Ultra Virus Killer (UVK)/update.bat
+++ USB-Tool_Scripts/_Data/Apps/Ultra Virus Killer (UVK)/update.bat
@@ -2,12 +2,56 @@
 
 title App Updater
 
-set "app=Ultra Virus Killer (UVK)"
-set "download=https://www.carifred.com/uvk/UVK.zip"
+echo Ultra Virus Killer (UVK)
+echo ----------------------------------------------------------------------------------------------------
 
-echo Downloading latest build of %app%...
+set "download=https://www.carifred.com/uvk/UVKPortable.exe"
+set "update=update.tmp"
+set "app=UVKPortable.exe"
+
+echo:
+echo Downloading 7zip (required for update)...
+wget https://www.7-zip.org/a/7zr.exe -P %TEMP%
+set "zip=%TEMP%\7zr.exe"
+
+echo:
+echo Downloading update...
+del %update%
+wget %download% -O %update%
+
+echo:
+echo Applying update...
+del %app%
+ren %update% %app%
+
+echo:
+echo Updating version file...
+del *.version
+for /f "usebackq tokens=*" %%i in (`powershell -command "$version = (Get-Item '%app%').VersionInfo.FileVersionRaw; $version -replace '\s+', '.'"`) do set "version=%%i"
+for /f "usebackq tokens=*" %%i in (`powershell -command "(Get-Item '%app%').LastWriteTime.ToString('yyyy-MM-dd')"`) do set "date=%%i"
+type NUL > v%version%_%date%.version
+
+
+echo:
+echo Updating checksums...
+del checksums.sfv
+set "hashutils=hashutils-1.2.4-redist"
+wget https://code.kliu.org/misc/hashutils/%hashutils%.7z -P %TEMP%
+%zip% x %TEMP%\%hashutils%.7z -o%TEMP%\%hashutils% -aoa
+%TEMP%\%hashutils%\bin.x86-64\crc32sum.exe -r * > checksums.sfv
+more +1 checksums.sfv > checksums.sfv.tmp
+del checksums.sfv
+ren checksums.sfv.tmp checksums.sfv
+
+echo:
+echo Updating parity data...
+del *.par2
+set "par2ver=1.0.0"
+wget https://github.com/Parchive/par2cmdline/releases/download/v%par2ver%/par2cmdline-%par2ver%-win-x64.zip -P %TEMP%
+tar -xf %TEMP%\par2cmdline-%par2ver%-win-x64.zip -C %TEMP%
+par2 c -v -b5000 -r5 -n1 -R parity.par2 *
 
 echo:
 echo End of script
-pause
+rem pause
 exit /b
