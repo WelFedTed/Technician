70ef6a9f09631c4592ec190a4f7d4453d7bb1dc8
COPS-SB <145305405+COPS-SB@users.noreply.github.com>
2025-08-01 09:28:44 +1000

    Update maintenance.bat

--- USB-Tool_Scripts/_Data/Scripts/maintenance.bat
+++ USB-Tool_Scripts/_Data/Scripts/maintenance.bat
@@ -1,5 +1,9 @@
 @echo off
 
+echo Maintenance Script
+echo --------------------------------------------------------------------------------
+
+
 winget source update
 winget upgrade --all --silent
 
@@ -9,6 +13,10 @@ sfc /scannow
 dism /online /cleanup-image /startcomponentcleanup /resetbase
 dism /online /cleanup-image /restorehealth
 sfc /scannow
+defrag /c /o
 chkdsk c: /r /scan /perf
 
+echo:
+echo End of script
+rem pause
 exit /b
