e7166157f83081553f38609fbd7f8db5da9be426
WelFedTed <34222508+WelFedTed@users.noreply.github.com>
2026-05-14 06:32:42 +1000

    created data transfer restore scripts

---
+++ USB-Tool_Scripts/DataTransfer_Restore.cmd
@@ -0,0 +1,93 @@
+@REM Checks if running as Admin, then passes the directory to "DataTransfer_Restore.ps1"
+@REM Run from folder you wish to import / restore from
+
+@echo off
+
+title Data Transfer - Restore Script
+
+echo:
+@REM draw line to window width
+for /f "usebackq delims=" %%W in (`powershell -command "&{(get-host).ui.rawui.WindowSize.Width;}"`) do set "window_width=%%W"
+for /L %%i in (1,1,%window_width%) do <nul set /p=-
+echo Data Transfer - Restore Script
+for /L %%i in (1,1,%window_width%) do <nul set /p=-
+echo:
+echo:
+
+@REM update DataTransfer_Restore.cmd
+echo Checking for latest version of DataTransfer_Restore.cmd...
+powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/WelFedTed/Technician/refs/heads/main/USB-Tool_Scripts/DataTransfer_Restore.cmd' -OutFile '%temp%\DataTransfer_Restore_latest.cmd'"
+fc "%temp%\DataTransfer_Restore_latest.cmd" "%~dp0\DataTransfer_Restore.cmd" >nul
+if errorlevel 1 (
+    echo A newer version of DataTransfer_Restore.cmd is available
+    echo Updating...
+    move /Y "%temp%\DataTransfer_Restore_latest.cmd" "%~dp0\DataTransfer_Restore.cmd" >nul
+    echo Restarting script...
+    "%~dp0\DataTransfer_Restore.cmd" %*
+    exit /b
+) else (
+    echo You already have the latest version of DataTransfer_Restore.cmd
+    echo Proceeding...
+    del "%temp%\DataTransfer_Restore_latest.cmd"
+)
+echo:
+
+@REM update DataTransfer_Restore.ps1
+echo Checking for latest version of DataTransfer_Restore.ps1...
+powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/WelFedTed/Technician/refs/heads/main/USB-Tool_Scripts/DataTransfer_Restore.ps1' -OutFile '%temp%\DataTransfer_Restore_latest.ps1'"
+fc "%temp%\DataTransfer_Restore_latest.ps1" "%~dp0\DataTransfer_Restore.ps1" >nul
+if errorlevel 1 (
+    echo A newer version of DataTransfer_Restore.ps1 is available
+    echo Updating...
+    move /Y "%temp%\DataTransfer_Restore_latest.ps1" "%~dp0\DataTransfer_Restore.ps1" >nul
+) else (
+    echo You already have the latest version of DataTransfer_Restore.ps1
+    echo Proceeding...
+    del "%temp%\DataTransfer_Restore_latest.ps1"
+)
+echo:
+
+echo Checking if you have Administrator permissions...
+echo:
+
+rem check if already elevated
+NET FILE 1>NUL 2>NUL
+if '%errorlevel%' == '0' ( goto gotPrivileges ) else ( goto getPrivileges )
+
+:getPrivileges
+rem not elevated, relaunch as admin
+pushd "%CD%"
+echo Not currently elevated, requesting Administrator access...
+powershell -command start-process -file %~f0 %cd% -verb runas
+exit /b
+
+:gotPrivileges
+if "%~1" == "" (
+    set "currentDir=%cd%"
+) else (
+    set "currentDir=%~1"
+)
+rem is elevated, continue script
+echo Running as Administrator, proceeding...
+echo:
+echo You are about to run the Data Transfer Restore script.
+echo Restore data will be imported from the following directory:
+echo:
+echo %currentDir%
+echo:
+choice /c YN /n /m "Are you sure you want to continue? (Y/N): "
+
+IF ERRORLEVEL 2 GOTO NO_CONTINUE
+IF ERRORLEVEL 1 GOTO CONTINUE
+
+:CONTINUE
+pushd "%~dp0"
+PowerShell.exe -Command "& {Start-Process PowerShell.exe -ArgumentList '-NonInteractive -NoProfile -ExecutionPolicy Bypass -File "%~dp0\%~n0.ps1 %currentDir%"' -Verb RunAs}"
+goto END
+
+:NO_CONTINUE
+echo Exiting...
+goto END
+
+:END
+exit /b
